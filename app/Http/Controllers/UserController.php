acbf879b064704ed6926b5855d905e696bf405df
afifahnisa17 <afifahnisa041@gmail.com>
2025-03-05 08:29:57 +0700

    update userController menggunakan firstOrFail

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        $user= UserModel::findOrFail(1);
+        $user= UserModel::where('username', 'manager9')->firstOrFail();
         return view ('user', ['data' => $user]);
     }
 }
