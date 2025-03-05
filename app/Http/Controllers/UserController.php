3f577deab50dc2efc06342b0e0be14219d69b881
afifahnisa17 <afifahnisa041@gmail.com>
2025-03-05 11:16:58 +0700

    add file for create

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,20 +9,23 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        $user = UserModel::create([
-            'username' => 'manager11',
-            'nama' => 'Manager11',
-            'password' => Hash::make('12345'),
-            'level_id' => 2,
+        $user = UserModel::all();
+        return view('user', ['data'=> $user]);
+    }
+
+    public function tambah(){
+        return view('user_tambah');
+    }
+
+    public function tambah_simpan(Request $request)
+    {
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash::make('$request->password'),
+            'level_id' => $request->level_id
         ]);
 
-        $user->username = 'manager12';
-        $user->save();
-
-        $user->wasChanged();
-        $user->wasChanged('username');
-        $user->wasChanged('username', 'level_id');
-        $user->wasChanged('nama');
-        dd($user->wasChanged(['nama', 'username']));
+        return redirect('/user');
     }
 }
