204b8d3cf07ff37b79ef2de2bb601a5b32640792
afifahnisa17 <afifahnisa041@gmail.com>
2025-03-05 08:00:54 +0700

    update userController, update userModel menggunakan metode where dan first

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        $user= UserModel::find(1);
+        $user= UserModel::where('level_id', 1)->first();
         return view ('user', ['data' => $user]);
     }
 }
