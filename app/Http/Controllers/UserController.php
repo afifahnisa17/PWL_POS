474fedcd8d6a09b1a9e17e7a819506e902b3fc6a
afifahnisa17 <afifahnisa041@gmail.com>
2025-03-05 08:18:43 +0700

    update userController findOr dengan ID 20

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        $user= UserModel::findOr(1, ['username', 'nama'], function(){
+        $user= UserModel::findOr(20, ['username', 'nama'], function(){
             abort(404);
         });
         return view ('user', ['data' => $user]);
