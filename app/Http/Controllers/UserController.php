69d4643f396b1b565bbe85a94065213a76862e93
afifahnisa17 <afifahnisa041@gmail.com>
2025-03-12 00:20:56 +0700

    add belongsto

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,10 +8,10 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index(){
-        $user = UserModel::all();
-        return view('user', ['data'=> $user]);
-    }
+    // public function index(){
+    //     $user = UserModel::all();
+    //     return view('user', ['data'=> $user]);
+    // }
 
     public function tambah(){
         return view('user_tambah');
@@ -55,5 +55,11 @@ class UserController extends Controller
         $user->delete();
 
         return redirect('/user');
+    }
+
+    public function index()
+    {
+        $user = UserModel::with('level')-> get();
+        dd($user);
     }
 }
