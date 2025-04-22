517fd863230dd8574e59eafda2622e1d90f78a05
afifahnisa17 <afifahnisa041@gmail.com>
2025-04-22 21:15:50 +0700

    add login page

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,15 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as Authenticable;
 
-class UserModel extends Model
+class UserModel extends Authenticable
 {
     use HasFactory;
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
 
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $fillable = ['level_id', 'username', 'nama', 'password', 'created_at', 'updaye'];
+
+    protected $hidden = ['password'];
+
+    protected $casts = ['password' => 'hashed'];
 
     public function level():BelongsTo
     {
