9d42ea5a0c3b9fc8633e1966e2caf4fcdf859549
afifahnisa17 <afifahnisa041@gmail.com>
2025-05-31 19:18:06 +0700

    update hash on image

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -25,13 +25,17 @@ class RegisterController extends Controller
             return response()->json($validator->errors(), 422);
         }
 
-        //creae user
+        // handle image upload
+        $image = $request->file('image');
+        $image->store('public/images');
+
+        //create user
         $user = UserModel::create([
             'username' => $request->username,
             'nama' => $request ->nama,
             'password' => bcrypt($request->password),
             'level_id' => $request->level_id,
-            'image' => $request->image
+            'image' => $image->hashName(),
         ]);
 
         //return response
