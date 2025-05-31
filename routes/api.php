894d8434a60affafffafa9c9169aeabe14c7b35c
afifahnisa17 <afifahnisa041@gmail.com>
2025-05-31 16:56:30 +0700

    add api CRUD api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\LevelController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,3 +25,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('/logout', \App\Http\Controllers\Api\LogoutController::class)->name('logout');
+
+//Level Route
+Route::get('/level', [LevelController::class, 'index']);
+Route::post('/level', [LevelController::class, 'store']);
+Route::get('/level/{level}', [LevelController::class, 'show']);
+Route::put('/level/{level}', [LevelController::class, 'update']);
+Route::delete('/level/{level}', [LevelController::class, 'destroy']);
