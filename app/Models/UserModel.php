2cdc6c7530f81a40e2ac6c364b959dfc4e75aba4
afifahnisa17 <afifahnisa041@gmail.com>
2025-05-31 19:11:42 +0700

    add api image user

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -23,7 +24,7 @@ class UserModel extends Authenticable implements JWTSubject
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
-    protected $fillable = ['level_id', 'username', 'nama', 'password', 'created_at', 'updaye'];
+    protected $fillable = ['level_id', 'username', 'nama', 'password', 'created_at', 'update_at', 'image'];
     protected $hidden = ['password'];
 
     protected $casts = ['password' => 'hashed'];
@@ -48,5 +49,12 @@ class UserModel extends Authenticable implements JWTSubject
     public function getRole()
     {
         return $this->level->level_kode;
+    }
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn($image) => url('/storage/posts/'. $image),
+        );
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\LevelController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\BarangController;
+use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\LogoutController;
+use App\Http\Controllers\Api\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,6 +26,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/register', \App\Http\Controllers\Api\RegisterController::class)->name('register');
+Route::post('/register1', \App\Http\Controllers\Api\RegisterController::class)->name('register1');
+
 Route::post('/login', \App\Http\Controllers\Api\LoginController::class)->name('login');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
